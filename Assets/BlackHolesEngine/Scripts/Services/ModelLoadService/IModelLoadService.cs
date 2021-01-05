4eb38e2e73e3953b249136269db920870fb06d2e
SaleyIlya <saley19997@gmail.com>
2021-01-06 00:59:48 +0300

    added model save/load service

---
+++ Assets/BlackHolesEngine/Scripts/Services/ModelLoadService/IModelLoadService.cs
@@ -0,0 +1,10 @@
+﻿using BlackHoles.BlackHolesEngine.Scripts.MVVM.Model;
+
+namespace BlackHoles.BlackHolesEngine.Scripts.Services.ModelLoadService
+{
+    public interface IModelLoadService
+    {
+        void SaveData(IModel model, string path);
+        void LoadData(IModel model, string path);
+    }
+}
---
+++ Assets/BlackHolesEngine/Scripts/Services/ModelLoadService/Implementation/LocalModelLoadService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using BlackHoles.BlackHolesEngine.Scripts.MVVM.Model;
+
+namespace BlackHoles.BlackHolesEngine.Scripts.Services.ModelLoadService.Implementation
+{
+    public class LocalModelLoadService : IModelLoadService
+    {
+        public void SaveData(IModel model, string path)
+        {
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+            }
+
+            var file = File.Create(path);
+            var stringData = model.GetDataToSave();
+            var bytesArray = Encoding.UTF8.GetBytes(stringData);
+            file.Write(bytesArray, 0, bytesArray.Length);
+        }
+
+        public void LoadData(IModel model, string path)
+        {
+            if (File.Exists(path))
+            {
+                var data = File.ReadAllText(path);
+                model.InitData(data);
+                return;
+            }
+            
+            model.InitData();
+        }
+    }
+}
