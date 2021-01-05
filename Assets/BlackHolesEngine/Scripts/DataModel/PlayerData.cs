60e01ef3dbcd6ccdf64ecf3a6b15ebebf3004086
SaleyIlya <saley19997@gmail.com>
2021-01-06 01:12:36 +0300

    added PlayerData

---
+++ Assets/BlackHolesEngine/Scripts/DataModel/PlayerData.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace BlackHoles.BlackHolesEngine.Scripts.DataModel
+{
+    public class PlayerData
+    {
+        [JsonProperty("player")]
+        public Player Player { get; set; }
+        [JsonProperty("user")]
+        public User User { get; set; }
+    }
+}
