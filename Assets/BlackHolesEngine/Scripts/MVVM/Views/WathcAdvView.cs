5d7467b7e34524c96639e1169090f6b8ebc06afd
SaleyIlya <saley19997@gmail.com>
2021-01-08 19:31:42 +0300

    added watch adv panel

---
+++ Assets/BlackHolesEngine/Scripts/MVVM/Views/WathcAdvView.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace BlackHoles.BlackHolesEngine.Scripts.MVVM.Views
+{
+    public class WathcAdvView : MonoBehaviour
+    {
+        [SerializeField] private Button closeButton;
+        [SerializeField] private Button watchButton;
+        [SerializeField] private GameObject watchAdvViewObj;
+        [Header("Prefabs")]
+        [SerializeField] private GameObject advPanelView;
+
+        private void Start()
+        {
+            closeButton.onClick.AddListener(ClosePanel);
+            watchButton.onClick.AddListener(StartWatchAdv);
+        }
+
+        private void ClosePanel()
+        {
+            Destroy(watchAdvViewObj);
+        }
+
+        private void StartWatchAdv()
+        {
+            Instantiate(advPanelView);
+            ClosePanel();
+        }
+    }
+}
