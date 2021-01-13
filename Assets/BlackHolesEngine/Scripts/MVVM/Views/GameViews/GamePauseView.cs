ef78452b85df45fd5377f65534fb9e76103f0bb2
SaleyIlya <saley19997@gmail.com>
2021-01-13 03:57:51 +0300

    added game pause view

--- Assets/BlackHolesEngine/Scripts/MVVM/Views/GameViews/GamePauseView.cs
+++ Assets/BlackHolesEngine/Scripts/MVVM/Views/GameViews/GamePauseView.cs
@@ -1,9 +1,40 @@
-﻿using UnityEngine;
+﻿using System;
+using BlackHoles.BlackHolesEngine.Scripts.Core.ServiceLocator;
+using BlackHoles.BlackHolesEngine.Scripts.MVVM.ViewModels;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace BlackHoles.BlackHolesEngine.Scripts.MVVM.Views.GameViews
 {
     public class GamePauseView : MonoBehaviour
     {
+        [SerializeField] private Button menuButton;
+        [SerializeField] private Button unpauseButton;
+        [Space]
+        [SerializeField] private GameObject gamePauseViewGameObject;
+        [Space]
+        [SerializeField] private string menuScene;
         
+        private GameViewModel _viewModel;
+
+        private void Start()
+        {
+            _viewModel = ServiceLocator.Default.Resolve<GameViewModel>();
+            
+            menuButton.onClick.AddListener(LoadMainMenu);
+            unpauseButton.onClick.AddListener(UnpauseGame);
+        }
+
+        private void UnpauseGame()
+        {
+            _viewModel.SetPauseCommand.Execute(false);
+            Destroy(gamePauseViewGameObject);
+        }
+
+        private void LoadMainMenu()
+        {
+            SceneManager.LoadScene(menuScene);
+        }
     }
 }
